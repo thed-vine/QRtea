650a560594f8eb1a91c5c8ff0c8d1f681bf29d55
Divine Enyinnayah <enyinnayahd@gmail.com>
2026-04-14 04:14:57 +0100

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import QRCode from "react-qr-code";
 
 export default function Home() {
+  const [link, setLink] = useState("");
+  const [qrCode, setQrCode] = useState("");
+
+  const generateQRCode = () => {
+    setQrCode(link);
+    alert("QR Code generated successfully!");
+  };
+
   return (
     <>
       <main>
@@ -8,9 +18,20 @@ export default function Home() {
           <h2 className="text-2xl font-bold mb-4">Welcome to QRtea</h2>
         </div>
         <div>
-          <input type="text" placeholder="Enter the link" />
-          <button>Generate QR Code</button>
+          <input
+            type="text"
+            placeholder="Enter the link"
+            value={link}
+            onChange={(e) => setLink(e.target.value)}
+          />
+          <button onClick={generateQRCode}>Generate QR Code</button>
         </div>
+        {qrCode && (
+          <div className="mt-4">
+            <h3 className="text-lg font-semibold mb-2">Your QR Code:</h3>
+            <QRCode value={qrCode} size={200} />
+          </div>
+        )}
       </main>
     </>
   );
